2f41ee2c10cebf617aabe023fcef318dd8b202f4
Mimi Chenyao <mimichenyao@gmail.com>
2017-11-23 15:41:05 -0500

    Create VariablesAndConstants.swift

---
+++ Variables and Constants/VariablesAndConstants.swift
@@ -0,0 +1,15 @@
+// VariablesAndConstants.swift
+// Created by Mimi Chenyao for the Get Swifty blog series
+// Original post here: http://asianbarbie.com/swift-variables-constants/
+
+// Some of this code may not work. Please comment and un-comment in a Playground to test true functionality.
+
+// First example
+
+var dressColor: String = "yellow"
+dressColor = "green"
+
+// Second example
+
+let immutableDressColor: String = "red"
+immutableDressColor = "blue"
