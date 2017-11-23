b4605fe05c6d0b6650c8e4d8ee1a3b64f0993bc2
Mimi Chenyao <mimichenyao@gmail.com>
2017-11-23 15:44:42 -0500

    Update VariablesAndConstants.swift

--- Variables and Constants/VariablesAndConstants.swift
+++ Variables and Constants/VariablesAndConstants.swift
@@ -4,12 +4,40 @@
 
 // Some of this code may not work. Please comment and un-comment in a Playground to test true functionality.
 
-// First example
+// Example 1
 
 var dressColor: String = "yellow"
 dressColor = "green"
 
-// Second example
+// Example 2
 
 let immutableDressColor: String = "red"
 immutableDressColor = "blue"
+
+// Example 3
+
+var dressColor: String = "yellow"
+dressColor = 15
+
+// Example 4
+
+var dressColor = "yellow"
+
+// Example 5
+
+var hairColor = "black"
+var eyeColor = "brown"
+var dressColor = "yellow"
+
+var hairColor = "black", eyeColor = "brown", dressColor = "yellow"
+
+// Example 6
+
+let 🐸☕️ = "This is a super annoying thing to do, but that's none of my biz"
+
+// Example 7
+
+var + = 9
+let hi-> = (7, "hello")
+var asian barbie = "mimi chenyao"
+let 90210 = "way too addictive of a show"
